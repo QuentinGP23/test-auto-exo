60e301618a3d468e6e080c0195b5cb4d99acecf0
QuentinGP <quentgeoffpro@yahoo.com>
2024-10-31 14:11:26 +0100

    affichage index

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import routes from './routes';
+import { getAllTasks } from './taskDatabase';
 
 const app = express();
 app.use(express.json());
 app.use('/api/tasks', routes);
+
+app.get('/', (req, res) => {
+  const tasks = getAllTasks();
+  res.json(tasks);
+});
 
 export default app;
